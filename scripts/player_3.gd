4c625cdf77a1f2da52d5076ad36ef131c7a57a34
WOCae <t-satoIZ@outlook.jp>
2024-02-19 22:03:15 +0900

    Update player_3.gd

--- scripts/player_3.gd
+++ scripts/player_3.gd
@@ -3,10 +3,16 @@ extends CharacterBody2D
 var screen_size # Size of the game window.
 
 @export var speed = 380
+@export var Shotcnt = 0.5 #射出間隔
+@export var ShotSpeed = 1000 #速度
+var cnt = 0
+const Ball = preload("res://Scene/ball.tscn") #ballシーンのプリロード
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	
+	position = Vector2(400, 1000)
+		
 func _physics_process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed(&"move_right"):
@@ -25,6 +31,23 @@ func _physics_process(delta):
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
 	move_and_slide()
+
+	#ballを作成する　スペースを押すとボールが作成されます。
+	if Input.is_action_pressed(("ui_select")) || Input.is_action_pressed(("shot")):
+		cnt += delta
+		if cnt > Shotcnt:
+			cnt -= Shotcnt
+		
+			# ball 
+			var Ball = Ball.instantiate()
+			Ball.position = position
+			
+			# ballの位置と速度
+			Ball.start(position.x, position.y, ShotSpeed)
+			
+			#ルートにインスタンスを追加
+			var mainNode = get_owner()
+			mainNode.add_child(Ball)
 
 
 func _on_player_3_area_entered(area):
