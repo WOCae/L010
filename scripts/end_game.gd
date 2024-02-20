3965b9dea98788647f87d5ddca725e9efaf6a989
WOCae <t-satoIZ@outlook.jp>
2024-02-20 12:34:01 +0900

    Update end_game.gd

--- scripts/end_game.gd
+++ scripts/end_game.gd
@@ -3,5 +3,5 @@ extends Control
 func _input(event):
 	if event.is_action_released("Quit"):
 		get_tree().quit()
-	elif event.is_action_released("ui_accept"):
+	elif event.is_action_released("ui_accept")|| Input.is_action_pressed(("start")):
 		get_tree().change_scene_to_file("res://Scene/start_game.tscn")
